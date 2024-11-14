0cc7407859b045a20dbd51c22496d92106c3c28a
Hilshat76 <hilshat76@gmail.com>
2024-11-14 11:48:35 +0300

    24.2 Сериализаторы. Доработка: ❌ В сериализатор курса добавлено поле для вывода урока. Это поле не нужно делать типом SerializerMethodField, в нем нужно сослаться на сериализатор уроков)

--- materials/serializers.py
+++ materials/serializers.py
@@ -9,22 +9,19 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class CourseDetailSerializer(serializers.ModelSerializer):
-    count_lessons = serializers.SerializerMethodField()
-    lessons = serializers.SerializerMethodField()
-
-    def get_count_lessons(self, course):
-        return Lesson.objects.filter(course=course).count()
-
-    def get_lessons(self, course):
-        return [lesson.title for lesson in Lesson.objects.filter(course=course)]
-
-    class Meta:
-        model = Course
-        fields = ('title', 'preview', 'description', 'owner', 'count_lessons', 'lessons',)
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseDetailSerializer(serializers.ModelSerializer):
+    count_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_count_lessons(self, course):
+        return Lesson.objects.filter(course=course).count()
+
+    class Meta:
+        model = Course
+        fields = ('title', 'preview', 'description', 'owner', 'count_lessons', 'lessons',)
