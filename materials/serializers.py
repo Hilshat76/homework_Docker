ca6013a7aacb390371cce218bb55c4ab04313cbb
Hilshat76 <hilshat76@gmail.com>
2024-11-21 21:39:14 +0300

    25.2 Валидаторы, пагинация и тесты. Домашнее задание.

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,17 +1,28 @@
 from rest_framework import serializers
 
-from materials.models import Course, Lesson
-
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
+from materials.models import Course, Lesson, Subscription
+from materials.validators import validate_youtube_link
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    linc_to_video = serializers.CharField(validators=[validate_youtube_link], read_only=True)
+
     class Meta:
         model = Lesson
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
+
+    def get_is_subscribed(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return Subscription.objects.filter(user=user, course=obj).exists()
+        return False
+
+    class Meta:
+        model = Course
         fields = '__all__'
 
 
@@ -25,3 +36,9 @@ class CourseDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ('title', 'preview', 'description', 'owner', 'count_lessons', 'lessons',)
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
--- materials/urls.py
+++ materials/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from materials.apps import MaterialsConfig
-from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView, SubscriptionAPIView
 
 app_name = MaterialsConfig.name
 
@@ -10,10 +11,11 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
-    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
-    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_detail'),
-    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
-    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-detail'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+    path('subscription/', SubscriptionAPIView.as_view(), name='subscription-create'),
 
 ] + router.urls
